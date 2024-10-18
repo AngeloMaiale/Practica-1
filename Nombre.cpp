18180d8c7ac0e14217698dcc0d9e648af1718284
Angelo <angelomaiale@gmail.com>
2024-10-18 10:53:20 -0400

    Hola

---
+++ Nombre.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+    int x=0;
+    int data
+    ofstream output("./data/data.txt");
+
+    if(! output.is_open()){
+        cout<<"No se puede crear ni abrir el archivo"<<endl;
+        return 0;
+    }
+    cout<<"Cuantos datos va a ingresar"<<endl;
+    cin>>x;
+
+    while(x>0){
+        cout<<"Ingrese un numero entero"<<endl;
+        cin>>data;
+        output<<data<<endl;
+        x--;
+    }
+}
